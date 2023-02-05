52f813acd1ab2a608846eb1e77244de1cf0bb84f
MoonStar <moonsung0331@gmail.com>
2023-02-05 18:22:51 +0900

    #6 - 2. 버블정렬

---
+++ src/inflearn/part6_sorting_searching/SortingAndSearching02.java
@@ -0,0 +1,33 @@
+package inflearn.part6_sorting_searching;
+
+import java.util.Scanner;
+
+class SortingAndSearching02 {
+    public int[] solution(int n , int[] arr) {
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<n-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        SortingAndSearching02 T = new SortingAndSearching02();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        for(int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
