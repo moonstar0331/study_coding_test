871bfaf4802426b4927765f4e930da62ae5ffc81
MoonStar <moonsung0331@gmail.com>
2023-03-24 19:39:52 +0900

    프로그래머스 Lv1 : 체육복

---
+++ src/programmers/Lv1/GymSuit.java
@@ -0,0 +1,36 @@
+package programmers.Lv1;
+
+import java.util.*;
+
+// 체육복
+public class GymSuit {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n-lost.length;
+
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        for(int i=0; i<lost.length; i++) {
+            for(int j=0; j<reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        for(int i=0; i<lost.length; i++) {
+            for(int j=0; j<reserve.length; j++) {
+                if((lost[i]-1 == reserve[j]) || (lost[i]+1==reserve[j])) {
+                    answer++;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
