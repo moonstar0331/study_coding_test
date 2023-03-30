495ffdd66e4ebe2d3f1ce53187b5a8c3dc79e4dc
MoonStar <moonsung0331@gmail.com>
2023-03-30 15:52:23 +0900

    프로그래머스 Lv1 : 옹알이(2)

---
+++ src/programmers/Lv1/Babbling2.java
@@ -0,0 +1,21 @@
+package programmers.Lv1;
+
+// Lv1 - 옹알이(2)
+public class Babbling2 {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(int i=0; i<babbling.length; i++) {
+            if(babbling[i].contains("ayaaya") || babbling[i].contains("yeye") || babbling[i].contains("woowoo") || babbling[i].contains("mama")) {
+                continue;
+            }
+
+            babbling[i] = babbling[i].replace("aya", " ");
+            babbling[i] = babbling[i].replace("ye", " ");
+            babbling[i] = babbling[i].replace("woo", " ");
+            babbling[i] = babbling[i].replace("ma", " ");
+            babbling[i] = babbling[i].replace(" ", "");
+            if(babbling[i].length() == 0) answer++;
+        }
+        return answer;
+    }
+}
