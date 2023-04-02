7d55e3bab8fc22dc65cfb4e37dc285b439da6c0b
MoonStar <moonsung0331@gmail.com>
2023-04-02 23:55:57 +0900

    프로그래머스 Lv2 : 다음 큰 숫자

---
+++ src/programmers/Lv2/NextBigNumber.java
@@ -0,0 +1,22 @@
+package programmers.Lv2;
+
+public class NextBigNumber {
+    public int solution(int n) {
+        int answer = 0;
+
+        int n_cnt = Integer.bitCount(n);
+        int b_cnt = 0;
+
+        while(true){
+            n++;
+            b_cnt = Integer.bitCount(n);
+            
+            if(n_cnt == b_cnt){
+                answer = n;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
