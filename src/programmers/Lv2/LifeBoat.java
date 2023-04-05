806b061e66c66c87464dc05d5a5346fb7459164c
MoonStar <moonsung0331@gmail.com>
2023-04-05 20:31:30 +0900

    프로그래머스 Lv2 : 구명보트 - 코드 개선

--- src/programmers/Lv2/LifeBoat.java
+++ src/programmers/Lv2/LifeBoat.java
@@ -6,30 +6,20 @@ import java.util.*;
 public class LifeBoat {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int[] check = new int[people.length];
 
         Arrays.sort(people);
 
         int lt = 0;
         int rt = people.length - 1;
         while(lt <= rt) {
-            if(check[lt] == 1) {
-                lt++;
-                continue;
-            } else if(check[rt] == 1) {
+            if(people[lt] + people[rt] > limit) {
+                answer++;
                 rt--;
                 continue;
             } else {
-                if(people[lt] + people[rt] > limit) {
-                    answer++;
-                    check[rt--] = 1;
-                    continue;
-                } else {
-                    answer++;
-                    check[lt++] = 1;
-                    check[rt--] = 1;
-                    continue;
-                }
+                answer++;
+                lt++;
+                rt--;
             }
         }
 
