575fbf1fdc727d320c4b20ff95d974d4e7c3628f
moonstar/김문성 <79830859+Kim-Moon-Sung@users.noreply.github.com>
2023-05-21 22:57:54 +0900

    [Silver IV] Title: 카드, Time: 400 ms, Memory: 33476 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,37 +1,31 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
-// 백준 11652 - 카드
 public class Main {
-    // Current Count : 지금 보고 있는 숫자가 등장한 횟수
-    // Mode Count : 지금까지의 최빈값의 등장 횟수
-    // mode : 지금까지의 최빈값
-
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
-
+    
     static int N;
     static long[] a;
-
+    
     static void input() {
         N = scan.nextInt();
-        a = new long[N + 1];
+        a = new long[N+1];
         for(int i=1; i<=N; i++) {
             a[i] = scan.nextLong();
         }
     }
-
+    
     static void pro() {
-        // Sort 정렬 하기
+        // Sort 정렬하기
         Arrays.sort(a, 1, N+1);
-
-        // mode : 최빈값, modeCnt : 최빈값의 등장 횟수, curCnt : 현재 값(a[1])의 등장 횟수
+        
+        // mode: 최빈값, modeCnt=최빈값의 등장횟수, curCnt=현재값의 등장횟수
         long mode = a[1];
-        int modeCnt = 1, curCnt = 1;
-
-        // 2 번 원소부터 차례대로 보면서, 같은 숫자가 이어서 나오고 있는지, 새로운 숫자가 나왔는 지를 판단하여
-        // curCnt 를 갱신해주고, 최빈값을 갱신하는 작업
+        int modeCnt=1, curCnt = 1;
+        
+        // 2번 원소부터 차례대로 보면서, 같은 숫자가 이어서 나오기 있는지, 새로운 숫자가 나왔는지 판단
+        // curCnt를 갱신해주고, 최빈값을 갱신하는 작업
         for(int i=2; i<=N; i++) {
             if(a[i] == a[i-1]) {
                 curCnt++;
@@ -43,20 +37,20 @@ public class Main {
                 mode = a[i];
             }
         }
-
-        // 정답 출력하기 - mode
+        
+        // 정답 출력
         System.out.println(mode);
     }
-
+    
     public static void main(String[] args) {
         input();
         pro();
     }
-
+    
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
-
+        
         public FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
@@ -64,12 +58,12 @@ public class Main {
         public FastReader(String s) throws FileNotFoundException {
             br = new BufferedReader(new FileReader(new File(s)));
         }
-
+        
         String next() {
-            while (st == null || !st.hasMoreElements()) {
+            while(st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
+                } catch(IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -79,20 +73,20 @@ public class Main {
         int nextInt() {
             return Integer.parseInt(next());
         }
-
+        
         long nextLong() {
             return Long.parseLong(next());
         }
-
+        
         double nextDouble() {
             return Double.parseDouble(next());
         }
-
+        
         String nextLine() {
             String str = "";
             try {
                 str = br.readLine();
-            } catch (IOException e) {
+            } catch(IOException e) {
                 e.printStackTrace();
             }
             return str;
