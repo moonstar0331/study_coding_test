e3d30d244d4793adf0b16808d7cd65fc6ceb4506
moonstar/김문성 <79830859+Kim-Moon-Sung@users.noreply.github.com>
2023-05-24 14:20:14 +0900

    [Gold IV] Title: 부분합, Time: 316 ms, Memory: 23648 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,58 +1,56 @@
 import java.io.*;
 import java.util.*;
 
-// 백준 1806 - 부분합
 public class Main {
-
-    static StringBuilder sb = new StringBuilder();
     static FastReader scan = new FastReader();
-
+    static StringBuilder sb = new StringBuilder();
+    
     static int n, S;
     static int[] a;
-
+    
     static void input() {
         n = scan.nextInt();
         S = scan.nextInt();
-        a = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
+        a = new int[n+1];
+        for(int i=1; i<=n; i++){
             a[i] = scan.nextInt();
         }
     }
-
+    
     static void pro() {
-        int R = 0, sum = 0, ans = n + 1;
-        for (int L = 1; L <= n; L++) {
-            // L - 1 을 구간에서 제외하기
-            sum -= a[L - 1];
-
+        int R=0, sum=0, ans=n+1;
+        
+        for(int L=1; L<=n; L++) {
+            // L-1 을 구간에서 제외하기
+            sum -= a[L-1];
+            
             // R 을 옮길 수 있을 때까지 옮기기
-            while (R + 1 <= n && sum < S) {
+            while(R + 1 <= n && sum < S) {
                 R++;
                 sum += a[R];
             }
-
-            // [L ... R] 의 합, 즉 sum 이 조건을 만족하면 정답 갱신하기
-            if (sum >= S) {
-                ans = Math.min(ans, R - L + 1);
-            }
+            
+            // [L..R] 의 합, 즉 sum이 조건을 만족하면 정답 갱신하기
+           if(sum >= S) {
+               ans = Math.min(ans, R-L+1);
+           }
         }
-
-        // ans 값을 보고 불가능 판단하기
-        if (ans == n + 1) {
+        
+        if(ans == n+1) {
             ans = 0;
         }
         System.out.println(ans);
     }
-
+    
     public static void main(String[] args) {
         input();
         pro();
     }
-
+    
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
-
+        
         public FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
@@ -60,12 +58,12 @@ public class Main {
         public FastReader(String s) throws FileNotFoundException {
             br = new BufferedReader(new FileReader(new File(s)));
         }
-
+        
         String next() {
-            while (st == null || !st.hasMoreElements()) {
+            while(st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
+                } catch(IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -75,20 +73,20 @@ public class Main {
         int nextInt() {
             return Integer.parseInt(next());
         }
-
+        
         long nextLong() {
             return Long.parseLong(next());
         }
-
+        
         double nextDouble() {
             return Double.parseDouble(next());
         }
-
+        
         String nextLine() {
             String str = "";
             try {
                 str = br.readLine();
-            } catch (IOException e) {
+            } catch(IOException e) {
                 e.printStackTrace();
             }
             return str;
