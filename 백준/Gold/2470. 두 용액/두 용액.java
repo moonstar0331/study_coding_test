4e284d78ef663874ac3018f65b735ded64fec495
moonstar/김문성 <79830859+Kim-Moon-Sung@users.noreply.github.com>
2023-05-24 14:27:50 +0900

    [Gold V] Title: 두 용액, Time: 376 ms, Memory: 28256 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,59 +1,53 @@
 import java.io.*;
 import java.util.*;
 
-// 백준 2470 - 두 용액
 public class Main {
-
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
-
+    
     static int N;
     static int[] A;
-
+    
     static void input() {
         N = scan.nextInt();
-        A = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+        A = new int[N+1];
+        for(int i=1; i<=N; i++) {
             A[i] = scan.nextInt();
         }
     }
-
+    
     static void pro() {
-        // A 에 대해 이분 탐색을 할 예정이니까, 정렬을 미리 해주자.
-        Arrays.sort(A, 1, N + 1);
-
+        // A 에 대해 이분 탐색을 할 예정이닌깐, 정렬
+        Arrays.sort(A, 1, N+1);
+        
         int best_sum = Integer.MAX_VALUE;
-        int v1 = 0, v2 = 0, L = 1, R = N;
-
-        // L == R 인 상황이면 용액이 한 개 뿐인 것이므로, L < R 일 때까지만 반복한다.
+        int v1=0, v2=0, L=1, R=N;
+        
         while(L < R) {
             int sum = A[L] + A[R];
-            if (Math.abs(sum) < best_sum) {
+            if(Math.abs(sum) < best_sum) {
                 best_sum = Math.abs(sum);
                 v1 = A[L];
                 v2 = A[R];
             }
-
-            if(sum > 0) {
+            
+            if(sum>0) {
                 R--;
-            } else {
-                L++;
-            }
+            } else L++;
         }
         sb.append(v1).append(' ').append(v2);
         System.out.println(sb);
     }
-
+    
     public static void main(String[] args) {
         input();
         pro();
     }
-
-
+    
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
-
+        
         public FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
@@ -61,12 +55,12 @@ public class Main {
         public FastReader(String s) throws FileNotFoundException {
             br = new BufferedReader(new FileReader(new File(s)));
         }
-
+        
         String next() {
-            while (st == null || !st.hasMoreElements()) {
+            while(st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
+                } catch(IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -76,20 +70,20 @@ public class Main {
         int nextInt() {
             return Integer.parseInt(next());
         }
-
+        
         long nextLong() {
             return Long.parseLong(next());
         }
-
+        
         double nextDouble() {
             return Double.parseDouble(next());
         }
-
+        
         String nextLine() {
             String str = "";
             try {
                 str = br.readLine();
-            } catch (IOException e) {
+            } catch(IOException e) {
                 e.printStackTrace();
             }
             return str;
